86f7dc68699ffc6dcd3a6879af20e729cce0ae16
titaniumkun <y.alex1205orange93@gmail.com>
2021-10-22 10:49:31 +0900

    First commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -29,19 +29,11 @@ const Layout = ({ children }) => {
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
+          maxWidth: '100%',
         }}
       >
         <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+        <footer>
         </footer>
       </div>
     </>
